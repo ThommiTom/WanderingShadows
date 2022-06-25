d654505f68a67482c5cf01687eea461f4a70ce9c
schatton <thomas.schatton@fkfs.de>
2022-06-25 10:06:15 +0200

    first commit

---
+++ WanderingShadows/WanderingShadows/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WanderingShadows
+//
+//  Created by Thomas Schatton on 25.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WanderingShadows/WanderingShadows/WanderingShadowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderingShadowsApp.swift
+//  WanderingShadows
+//
+//  Created by Thomas Schatton on 25.06.22.
+//
+
+import SwiftUI
+
+@main
+struct WanderingShadowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
